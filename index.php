5bcb2225dd2259b1b9847ea4f16738be0378f2ed
gentianashabani <75908729+gentianashabani@users.noreply.github.com>
2021-05-21 00:36:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -425,7 +425,7 @@
   </section>
 
   <div class="kontakti">
-    <form action="kontakto.html">
+    <form action="kontakto.php">
       <input type="submit" value="Na kontaktoni" id="kontaktiInput" />
     </form>
   </div>
