3e8cada7662ee2f56e2688da42d56df5e280d4e3
gentianashabani <75908729+gentianashabani@users.noreply.github.com>
2021-05-20 20:52:17 +0200

    Update kontakto.php

--- kontakto.php
+++ kontakto.php
@@ -9,20 +9,24 @@
 			.error {color: #FF0000;}
 
 			* {
-				margin-top: 20px;
+				
 				box-sizing: border-box;
 			}
 
 			body{
 
-				background-color: aliceblue;
+			   /*background-color: #334c68;*/
+			   color: white;
+			   background: rgb(2, 0, 36);
+			   background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 0%, rgba(0, 212, 255, 1) 100%);
 			}
 
 			.container {
-				background-color: white;
+				/*background-color: white;*/
 				max-width: 400px;
 				width: 100%;
 				margin: auto;
+				margin-top: 100px;
 		    }
 		    #contact input[type="text"],
 			#contact input[type="number"],
@@ -31,11 +35,25 @@
 			#contact button[type="submit"] {
 				font: 400 12px/16px "Open Sans", Helvetica, Arial, sans-serif;
 			}
+
+			#contact input[type="text"],
+			#contact input[type="number"],
+			#contact input[type="gender"],
+			#contact input[type="radio"],
+			#contact textarea {
+				border-style: inset;
+			}
+
+			#contact textarea {
+				height: 90px;
+    			max-width: 70%;
+    			resize: none;
+			}
 			#contact input[type="submit"] {
 				cursor: pointer;
 				width: 100%;
 				border: none;
-				background: #000;
+				background: rgb(0, 0, 0);
 				color: #FFF;
 				margin: 0 0 5px;
 				padding: 25px 10px;
@@ -114,22 +132,25 @@
 			<div>
 				<form method="post" action="mailto:punaideale@gmail.com" id="contact">
 					<h4 style="font-size: 20px;">Na kontaktoni për të kërkuar puntorë</h4>
-					<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-					  Name: <input type="text" name="name" value="<?php echo $name;?>">
+					<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+					  <label for="name">Name:</label><br> 
+					  <input type="text" name="name" value="<?php echo $name;?>">
 					  <span class="error">* <?php echo $nameErr;?></span>
 					  <br><br>
-					  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+					  <label for="email">E-mail:</label><br>
+					  <input type="text" name="email" value="<?php echo $email;?>">
 					  <span class="error">* <?php echo $emailErr;?></span>
 					  <br><br>
-					  Telephone: <input type="number" name="telephone" value="<?php echo $telephone;?>">
+					  <label for="number">Telephone:</label><br>
+					  <input type="number" name="telephone" value="<?php echo $telephone;?>">
 					  <span class="error"><?php echo $telephoneErr;?></span>
 					  <br><br>
-					  Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+					  <label for="comment">Comment:</label><br>
+					  <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
 					  <br><br>
 					  Gender:
 					  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-					  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-					  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+					  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male  
 					  <span class="error">* <?php echo $genderErr;?></span>
 					  <br><br>
 					  <input type="submit" name="submit" value="Dërgo">   
