ff1159817a84fe64c15e39fc497e36a04717c46a
festinagashi <75373613+festinagashi@users.noreply.github.com>
2021-05-21 15:29:41 +0200

    Add files via upload

---
+++ validate.php
@@ -0,0 +1,23 @@
+<?php
+$myemail="tina@dermaku.com";
+$mypass="12345";
+
+if(isset($_POST['login'])){
+    $email=$_POST['email'];
+    $pass=$_POST['password'];
+    if($email==$myemail and $pass==$mypass){
+        if(isset($_POST['remember']) ){
+            setcookie('email', $email, time()+60*60*7);
+            setcookie('pass', $pass, time()+60*60*7);
+        }
+            session_start();
+            $_SESSION['email']=$email;
+            header("location: index.php");
+    }else{
+        echo "Email or Password is invalid. <br> click here to <a href='login.php'> Try again! </a>";
+    }
+}
+    else{
+        header("location: login.php");
+    }
+?>
