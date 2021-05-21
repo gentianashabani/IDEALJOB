5a2e7b0bbff31d08fea984d8d0994fafa89cf54c
festinagashi <75373613+festinagashi@users.noreply.github.com>
2021-05-21 15:56:28 +0200

    Update singup.php

--- singup.php
+++ singup.php
@@ -68,7 +68,7 @@ body {
 	</style>
 	</head>
 	<body>
-    <a href="index.php" style="color: white;">Kthehu ne kryefaqe</a>
+    <a href="login.php" style="color: white;">If you alredy have an account, get back to log in by clicking here</a>
 		<div class="login">
 			<h1>Sing up</h1>
 			<form action="connect.php" method="post">
